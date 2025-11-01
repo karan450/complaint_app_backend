c2e97ef9762dca87a8d737926e227764233ff86f
Keyur_Govani <79407939+karan450@users.noreply.github.com>
2025-11-01 11:11:29 +0530

    Remove CORS headers handling from fetchdata.php
    Removed CORS headers handling for OPTIONS requests.

--- fetchdata.php
+++ fetchdata.php
@@ -1,16 +1,4 @@
 <?php 
-    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Headers: *");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-    http_response_code(200);
-    exit(0);
-    }
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Headers: *");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-    header("Content-Type: application/json");
-
     include("./index.php");
     $data = json_decode(file_get_contents('php://input'));
 
